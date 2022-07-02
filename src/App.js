d845bb24b462d853a848075f73e9f782561ee7fe
MaquinoBSC <martinare.ipn@gmail.com>
2022-07-02 15:47:42 -0500

    Creacion de primer slice y acceso al mismo en el componente App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const tasks = useSelector(state => state.tasks);
+  console.log(tasks);
+
+  
   return (
     <div className="App">
       <h1>Hello World</h1>
---
+++ src/features/tasks/taskSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const tasksSlice = createSlice({
+    name: 'tasks',
+    initialState: [],
+    reducers: {
+        // Funciones para manipular el estado tasks
+    }
+});
+
+export default tasksSlice.reducer;
